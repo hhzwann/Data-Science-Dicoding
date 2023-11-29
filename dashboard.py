ea057ea7872cc2c15247ee733efe23ebc082333b
hhzwann <93906566+hhzwann@users.noreply.github.com>
2023-11-29 13:09:41 +0700

    first commit

---
+++ dashboard.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
---
+++ hello_world.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+ 
+st.write(
+    """
+    # saya Hazwan, and this is My first app
+    Hello, para calon praktisi data masa depan!
+    """
+)
